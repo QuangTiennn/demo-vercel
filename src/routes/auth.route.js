7f973541603ded3fd5ebaab97e76e2038b261208
quangtien <nguyenquangtien@digitalunicorn.fr>
2023-07-21 14:41:40 +0700

    deploy

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,8 +1,5 @@
 import { Router } from "express";
-import {
-  loginController,
-  registerController,
-} from "../controller/auth.controller";
+import { loginController, registerController } from "../controller";
 
 // Initialization
 const authRoute = Router();
