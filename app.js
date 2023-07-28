43fb9d9c7896d6aaa6906313d429811c7d1d43b4
quangtien <nguyenquangtien@digitalunicorn.fr>
2023-07-28 09:17:14 +0700

    deploy

--- app.js
+++ app.js
@@ -49,7 +49,9 @@ const options = {
     servers: [
       {
         url: "http://localhost:3000/api/v1",
-        // url: "http://178.128.110.134:3000/api/v1",
+      },
+      {
+        url: "http://178.128.110.134:3000/api/v1",
       },
     ],
   },
