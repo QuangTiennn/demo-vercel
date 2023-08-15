faa7b410ee5360a327bb24ae34b0cf691d54a863
quangtien <nguyenquangtien@digitalunicorn.fr>
2023-08-15 03:25:19 -0400

    upload image

--- api/routes/file.route.js
+++ api/routes/file.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
-import { uploadImageController } from "../controller/file.controller";
+import { uploadImageController } from "../controller/file.controller.js";
 const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
